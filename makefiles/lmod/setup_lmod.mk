6d15de8a7ae9e3395d62f1a11cc5be2a80352498
Holger Hoefling <hhoeflin@gmail.com>
2023-04-23 20:44:35 +0200

    fix erroneous ;

--- makefiles/lmod/setup_lmod.mk
+++ makefiles/lmod/setup_lmod.mk
@@ -1,5 +1,5 @@
 ifdef GLOBAL_MODULES
-LOAD_MODULES=module load ${GLOBAL_MODULES}
+LOAD_MODULES=module load ${GLOBAL_MODULES};
 endif
 # this way all loaded modules will stay active; globally needed modules can be set before running make
-INIT_LMOD=if [[ -z $${MODULEPATH} ]]; then . ${PREFIX}/lmod/lmod/init/bash; else . ${PREFIX}/lmod/lmod/init/profile; fi; module --force purge; export MODULEPATH=${PREFIX}/modules$${MODULEPATH:+:$${MODULEPATH}}; ${LOAD_MODULES};
+INIT_LMOD=if [[ -z $${MODULEPATH} ]]; then . ${PREFIX}/lmod/lmod/init/bash; else . ${PREFIX}/lmod/lmod/init/profile; fi; module --force purge; export MODULEPATH=${PREFIX}/modules$${MODULEPATH:+:$${MODULEPATH}}; ${LOAD_MODULES}
