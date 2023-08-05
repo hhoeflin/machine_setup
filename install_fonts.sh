e988bbb3e3a365b692798f0cb8eb1272105f16ed
Holger Hoefling <hhoeflin@gmail.com>
2023-08-05 23:29:04 +0200

    update the version of nerdfonts

--- install_fonts.sh
+++ install_fonts.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
+# for list of interesting fonts see 
+# https://realdougwilson.com/writing/coding-with-character
 declare -a fonts=(
 	FiraCode
 	FiraMono
 	Inconsolate
 	UbuntuMono
 	VictorMono
+	CascadiaCode
+	Iosevka
 )
 
-version='2.2.2'
+version='3.0.2'
 fonts_dir="${HOME}/.local/share/fonts"
 
 if [[ ! -d "$fonts_dir" ]]; then
@@ -18,10 +22,10 @@ fi
 for font in "${fonts[@]}"; do
     zip_file="${font}.zip"
     download_url="https://github.com/ryanoasis/nerd-fonts/releases/download/v${version}/${zip_file}"
-    echo "Downloading $download_url"
+    echo "Downloading ${download_url}"
     wget "$download_url"
-    unzip "$zip_file" -d "$fonts_dir" -x "*.txt/*" -x "*.md/*"
-    rm "$zip_file"
+    unzip "${zip_file}" -d "${fonts_dir}/${font}" -x "*.txt/*" -x "*.md/*"
+    rm "${zip_file}"
 done
 
 find "$fonts_dir" -name '*Windows Compatible*' -delete
