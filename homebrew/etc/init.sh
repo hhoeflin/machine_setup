f239c29411a9c4fe616340fc536fa327ac01b88d
Holger Hoefling <hhoeflin@gmail.com>
2025-04-26 13:50:01 +0200

    Some updates for mac

--- homebrew/etc/init.sh
+++ homebrew/etc/init.sh
@@ -1,13 +1,35 @@
-# source ble.sh if homebrew is installed
-if [[ -v HOMEBREW_PREFIX ]]; then
-    if [[ -f ${HOMEBREW_PREFIX}/etc/profile.d/ble.sh ]]; then
-        source ${HOMEBREW_PREFIX}/etc/profile.d/ble.sh
+# set vars for homebrew itself
+export HOMEBREW_NO_ANALYTICS=1
+if ! [[ -v HOMEBREW_PREFIX ]]; then
+    # variable unset; we auto-detect
+    if [[ -d "/opt/homebrew" ]]; then
+        HOMEBREW_PREFIX="/opt/homebrew"
+    elif [[ -d "${HOME}/.homebrew" ]]; then
+        HOMEBREW_PREFIX="${HOME}/.homebrew"
     else
-        echo "ble.sh not found in ${HOMEBREW_PREFIX}/etc/profile.d"
+        echo "HOMEBREW_PREFIX not set and can't auto-detect"
+        exit 1
     fi
-else
-    echo "HOMEBREW_PREFIX is not set"
 fi
+eval "$(${HOMEBREW_PREFIX}/bin/brew shellenv)"
+
+
+# source ble.sh if homebrew is installed
+BLE_INIT_SCRIPT=${HOMEBREW_PREFIX}/etc/profile.d/ble.sh 
+if [[ -f ${BLE_INIT_SCRIPT} ]]; then
+    source ${BLE_INIT_SCRIPT}
+else
+    echo "${BLE_INIT_SCRIPT} not found."
+fi
+
+# source lmod if is installed
+LMOD_INIT_SCRIPT=${HOMEBREW_PREFIX}/opt/lmod/init/profile
+if [[ -f ${LMOD_INIT_SCRIPT} ]]; then
+    source ${LMOD_INIT_SCRIPT}
+else
+    echo "${LMOD_INIT_SCRIPT} not found."
+fi
+
 
 eval "$(atuin init bash)"
 eval "$(zoxide init bash)"
